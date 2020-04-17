af3e97675f9001893ea45ab04eddfad9305ba551
tgzzq <3471116945@qq.com>
2020-04-17 17:23:26 +0800

    RuoYiApplication启动类 提示程序Profile，ip，prot信息

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -1,8 +1,15 @@
 package com.ruoyi;
 
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+
+import java.net.InetAddress;
 
 /**
  * 启动程序
@@ -10,21 +17,29 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author ruoyi
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+
 public class RuoYiApplication
 {
     public static void main(String[] args)
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
-        SpringApplication.run(RuoYiApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+        ConfigurableApplicationContext context = SpringApplication.run(RuoYiApplication.class, args);
+        ConfigurableEnvironment env = context.getEnvironment();
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String contextPath = "";
+        if (env.getProperty("server.servlet.contextPath") != null) {
+            contextPath = env.getProperty("server.servlet.contextPath");
+        }
+        String profiles = StringUtils.arrayToCommaDelimitedString(context.getEnvironment().getActiveProfiles());
+        System.out.println("\n----------------------------------------------------------" +
+                "\n\tApplication "+env.getProperty("ruoyi.name")+" is running! Access URLs:" +
+                "\n\tLocal: \t\t"+protocol+"://localhost:"+env.getProperty("server.port")+"" +
+                "\n\tExternal: \t"+protocol+"://"+InetAddress.getLoopbackAddress().getHostAddress()+":"+env.getProperty("server.port")+"" +
+                "\n\tProfile(s): \t"+profiles+"\n----------------------------------------------------------");
+
+
     }
 }
